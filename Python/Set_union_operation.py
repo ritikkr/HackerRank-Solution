c58b7b18790312df255ab1521c33b1a08b5e2dfb
Ritik Kumar <ritikkumar2405199@gmail.com>
2020-05-29 21:15:20 +0530

    Create Set_union_operation.py

---
+++ Python/Set_union_operation.py
@@ -0,0 +1,6 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n=int(input())
+n_roll= set(map(int, input().split()))
+b=int(input())
+b_roll=set(map(int,input().split()))
+print(len(n_roll.union(b_roll)))
