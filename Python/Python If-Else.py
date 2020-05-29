8fcd3f18f64e69c76532431a3bc9f22ca57c5588
Ritik Kumar <ritikkumar2405199@gmail.com>
2020-05-29 19:46:59 +0530

    Create Python If-Else.py

---
+++ Python/Python If-Else.py
@@ -0,0 +1,14 @@
+import sys
+n=int(input())
+if(n>=1 or n<=100):
+
+    if(n%2==0 and (n>=2 and n<=5) ):
+        print("Not Weird")
+    elif(n%2==0 and (n>=6 and n<=20) ):
+        print("Weird")
+    elif(n%2==0 and n>20):
+        print("Not Weird")
+    else:
+        print("Weird")
+else:
+    sys.exit()
