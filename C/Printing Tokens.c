45f60d0e6fb3aaaa32701d3c33cac32b266e7766
Ritik Kumar <ritikkumar2405199@gmail.com>
2020-05-30 00:19:40 +0530

    Create Printing Tokens.c

---
+++ C/Printing Tokens.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+#define MAXC 1000
+
+int main(void) {
+
+  int c = 0;
+  size_t n = 0;
+
+ 
+
+  /* read up to 100 characters from stdin, print each word on a line */
+  while (n < MAXC && (c = getchar()) != EOF && c != '\n') {
+    if (c == ' ')
+      printf("\n");
+    else
+      printf("%c", c);
+    n++;
+  }
+  printf("\n");
+
+  if (n == MAXC) /* read and discard remaining chars in stdin */
+    while ((c = getchar()) != '\n' && c != EOF)
+      ;
+
+  return 0;
+}
